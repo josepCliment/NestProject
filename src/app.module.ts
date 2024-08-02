e08c3ad816ee087e9ae1e16f40f15487ae300b70
Josep Climent <josep.climent@VALENCIA.VIDAVIA>
2024-08-02 13:18:30 +0200

    Removed user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BlogModule } from './blog/blog.module';
-import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [AuthModule, BlogModule, UserModule, CommonModule],
+  imports: [AuthModule, BlogModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
